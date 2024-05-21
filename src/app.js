2c971173bcd6baab4515891f67e5edfeaea16d09
Pavel <web.js.ezh@gmail.com>
2024-05-21 23:24:12 +0300

    feat: add form validation

---
+++ src/app.js
@@ -0,0 +1,57 @@
+import * as Yup from 'yup';
+import onChange from 'on-change';
+import { subscribe, render } from './view.js';
+import i18next from 'i18next';
+
+
+const state = {
+  form: {
+    isError: null,
+    message: '',
+  },
+  feeds: [],
+};
+
+const watchedState = onChange(state, render)
+
+const validateUrl = async (url) => {
+  try {
+    Yup.string().trim().required().url().notOneOf(watchedState.feeds).validate(url);
+    return true
+  } catch {
+    return false
+  }
+};
+
+export const onSubmit = async (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target)
+  // console.log(formData.get('url'))
+  const url = formData.get('url')
+  const isValid = await validateUrl(url);
+  if (isValid) {
+    watchedState.form.isError = false;
+    watchedState.form.message = 'RSS успешно загружен';
+    watchedState.feeds.push(url);
+    //отправляем запрос на url
+  } else {
+    watchedState.form.isError = true;
+    watchedState.form.message = 'Ссылка должна быть валидным URL'
+  }
+}
+
+export const app = () => {
+  subscribe();
+}
+
+// const formSchema = object().shape({
+//   inputValue: string()
+//     .url(t("urlIncorrect"))
+//     .required(t("notEmpty"))
+//     .notOneOf(loadedFeeds, t("urlExist")),
+// });
+
+// const submit = document.querySelector('[type="submit"]');
+
+// const inputValue = document.querySelector(".form-control");
+// inputValue.addEventListener("input");
---
+++ src/view.js
@@ -0,0 +1,33 @@
+import { onSubmit } from './app.js';
+
+const input = document.querySelector('.form-control');
+const formMessageNode = document.querySelector('.feedback');
+const form = document.querySelector('.rss-form');
+
+// const submit = document.querySelector('[type="submit"]');
+// inputValue.addEventListener("input");
+
+export const subscribe = () => {
+    form.addEventListener('submit', onSubmit);
+}
+
+export const render = (path, newValue) => {
+    switch(path) {
+        case 'form.isError': 
+        if (newValue) {
+            input.classList.add('is-invalid');
+            formMessageNode.classList.remove('text-success');
+            formMessageNode.classList.add('text-danger')
+        } else {
+            input.classList.remove('is-invalid');
+            formMessageNode.classList.remove('text-danger');
+            formMessageNode.classList.add('text-success');
+            form.reset();
+            input.focus();
+        }
+        break
+        case 'form.massage':
+            formMessageNode.innerHTML = newValue;
+            break
+    }
+}
