52b9a442133f8dc560298bd249c13b7431fa762e
Pavel <web.js.ezh@gmail.com>
2024-08-20 11:20:30 +0300

    refactor: resolve linter errors, remove commented-out code

--- src/app.js
+++ src/app.js
@@ -7,103 +7,34 @@ import render from './view.js';
 import ru from './locales/ru.js';
 import parser from './parser.js';
 
-// get response
-// const getAxiosResponse = (url) => {
-//   return axios.get(`https://allorigins.hexlet.app/get?url=${encodeURIComponent(url)}`)
-// .then((response) => {
-//   if (response.data.status === 200) {
-//     return response.data.contents;
-//   } else {
-//     console.error('Error getting data');
-//     return null;
-//   }
-// })
-// .catch((error) => {
-//   console.error('Error getting data', error);
-//   return null;
-// });
-// };
 const getAxiosResponse = (link) => axios.get(`https://allorigins.hexlet.app/get?disableCache=true&url=${encodeURIComponent(link)}`);
-
-// const url = 'https://ru.hexlet.io/lessons.rss';
-
-// getAxiosResponse(url).then((res)=> console.log(res))
 
 const updatePosts = (state) => {
   const existPosts = state.form.posts;
   const oldPosts = _.cloneDeep(existPosts);
-  // console.log('oldPosts', oldPosts);
-
   const url = state.form.field;
-  // console.log('url', url);
 
-  // Начало запроса данных
   getAxiosResponse(url)
     .then((data) => {
-    // console.log('data', data); // Здесь data должен быть результатом ответа от Axios
       parser(data.data.contents);
-    }) // Возвращаем данные, чтобы они пошли дальше по цепочке
+    })
     .then((newData) => {
-      // console.log('newData', newData); // Здесь newData должен быть результатом парсинга данных
-
       const newPosts = newData.posts;
-      // console.log('newPosts', newPosts);
-
       newPosts.forEach((newPost) => {
         const foundPosts = !oldPosts.find((oldPost) => oldPost.link === newPost.link);
-        // console.log('foundPosts', foundPosts);
-        // console.log('state.form.posts before push', state.form.posts);
 
         if (foundPosts) {
-          state.form.posts.push(newPost); // Исправлено: добавляем сам новый пост
+          state.form.posts.push(newPost);
         }
-
-        // console.log('state.form.posts after push', state.form.posts);
       });
     })
     .catch((error) => {
-      // console.error('Error:', error.message);
       state.form.errors = error.message;
     })
     .then(() => {
       setTimeout(() => updatePosts(state), 5000);
     });
 };
-
-//   const updatePosts = (state) => {
-//     const existPosts = state.form.posts;
-//     const oldPosts = _.cloneDeep(existPosts);
-//     const url = state.form.field;
-
-//     getAxiosResponse(url)
-//       .then((data) => parser(data))
-//       .then((newData) => {
-//         const newPosts = newData.posts;
-
-//         newPosts.forEach((newPost) => {  // Изменено с `map` на `forEach`
-//           const isNewPost = !oldPosts.find((oldPost) => oldPost.link === newPost.link);
-//           console.log('isNewPost', isNewPost)
-//           console.log('state.form.posts', state.form.posts)
-//           if (isNewPost) {
-//             state.form.posts.push(newPost); // Исправлено: добавляем сам новый пост
-//           }
-//         });
-//       })
-//       .catch((error) => {
-//         state.form.error.push(error.message);
-//       })
-//       .then(() => {
-//         setTimeout(() => updatePosts(state), 5000);
-//       });
-// };
-
-// const isValidRSS = (rssData) => {
-//   try {
-//     return parser(rssData).querySelector('rss') !== null;
-//   } catch (e) {
-//     return false;
-//   }
-// };
 
 const isValidRSS = (rssData) => {
   try {
@@ -140,14 +71,6 @@ const app = () => {
     },
   };
 
-  // const state = {
-  //   form: {
-  //     isError: null,
-  //     message: '',
-  //   },
-  //   feeds: [],
-  // };
-
   // step 3: init i18Next
   const i18Instance = i18next.createInstance();
   const defaultLang = 'ru';
@@ -171,7 +94,6 @@ const app = () => {
 
       // step 5: watch for state
       const watchedState = onChange(initialState, render(initialState, elements, i18Instance));
-      // console.log('watchedState', watchedState);
 
       const addFeeds = (id, title, description, state) => {
         state.form.feeds.push({ id, title, description });
@@ -187,7 +109,6 @@ const app = () => {
         }));
         state.form.posts = result.concat(watchedState.form.posts);
       };
-      // const watchedState = onChange(state, render)
 
       elements.form.addEventListener('submit', (e) => {
         e.preventDefault();
@@ -205,50 +126,25 @@ const app = () => {
           .string()
           .trim()
           .url(i18Instance.t('errors.invalidLink')) // instead of message of error - message from locales/ru.js
-          .notOneOf(watchedState.form.addedLinks, i18Instance.t('errors.addedLink')) // the same
+          .notOneOf(watchedState.form.addedLinks, i18Instance.t('errors.addedLink'))
           .validate(value) // check validation
           .then((url) => getAxiosResponse(url)) // return xmlDocument
-          // .then((responce)=> parser(responce.data.contents)) // return {feed, posts}
 
           .then((response) => {
-            // console.log('response', response);
-            if (!isValidRSS(response.data.contents)) { // Проверка на валидность RSS
-              throw new Error(i18Instance.t('errors.notRss')); // Если RSS не валиден, выбрасываем ошибку
+            if (!isValidRSS(response.data.contents)) {
+              throw new Error(i18Instance.t('errors.notRss')); // If RSS invalid - error
             }
-            return parser(response.data.contents); // Парсим RSS, return {feed, posts}
+            return parser(response.data.contents);
           })
-
-        // .then((response) => {
-        //   console.log('response:', response.status)
-        //   // if (response.status !== 200) { // Проверка на статус ответа
-        //   if (response.status === 0) { // Проверка на статус ответа
-        //     throw new Error(i18Instance.t('errors.notNetwork')); // Если статус не 200-ошибка
-        //   }
-        //   return response.data.contents; // Вернуть данные из ответа
-        // })
-        // .then((data) => {
-        //   if (!isValidRSS(data)) { // Проверка на валидность RSS
-        //     throw new Error(i18Instance.t('errors.notRss')); // Если RSS не валиден-ошибка
-        //   }
-        //   return parser(data); // Парсим RSS
-        // })
 
           .then((parsedRSS) => {
             const title = parsedRSS.feed.channelTitle;
-            // console.log(title)
             const description = parsedRSS.feed.channelDescription;
-            // console.log(description)
             const feedId = _.uniqueId();
-            // console.log('feedId', feedId )
-            // console.log(feedId)
 
-            // watchedState.form.feeds.push({ feedId, title, description });
             addFeeds(feedId, title, description, watchedState);
             addPosts(feedId, parsedRSS.posts, watchedState);
-            // console.log('parsedRSS', parsedRSS);
           })
-          // .then(() => updatePosts(watchedState))
-
           .then(() => { // in case - validation
             watchedState.form.valid = 'valid';
             watchedState.form.status = 'sending';
@@ -261,24 +157,9 @@ const app = () => {
           })
           .catch((error) => { // in case no-valid (if error is on during 'sending' or smth else)
             watchedState.form.valid = 'invalid';
-            watchedState.form.errors = error.message; // push error
-            // console.log('error.message', error.message);
+            watchedState.form.errors = error.message;
             watchedState.form.status = 'failed';
           })
-
-        // .catch((error) => {
-        //   watchedState.form.valid = 'invalid';
-        //   console.log('error.message', error.message)
-        //   if (error.message === 'Network Error') {
-        //     watchedState.form.errors = i18Instance.t('errors.notNetwork');
-        //   // } else if (error.message === 'notRss') {
-        //   //   watchedState.form.errors = i18Instance.t('errors.notRss');
-        //   } else {
-        //     watchedState.form.errors = error.message; // push last error
-        //   }
-        //   watchedState.form.status = 'failed';
-        // })
-
           .finally(() => {
             watchedState.form.status = 'filling';
           });
--- src/view.js
+++ src/view.js
@@ -3,7 +3,6 @@ const errorHandler = (elem, err) => {
   elements.input.classList.replace('is-valid', 'is-invalid');
   elements.feedback.classList.replace('text-success', 'text-danger');
   elements.feedback.textContent = err;
-//   console.log('err in view', err);
   elements.input.focus();
 };
 
@@ -52,11 +51,8 @@ const makeContainer = (elements, state, title, i18Instance) => {
       listGroupItem.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'align-items-start', 'border-0', 'border-end-0');
 
       const a = document.createElement('a');
-      // Проверка, прочитан ли пост
       const isRead = state.form.readPosts.includes(post.id);
       a.classList.add(isRead ? 'fw-normal' : 'fw-bold');
-
-      // a.classList.add('fw-bold')
       a.setAttribute('data-id', `${post.id}`);
       a.setAttribute('target', '_blank');
       a.setAttribute('href', `${post.link}`);
@@ -76,7 +72,6 @@ const makeContainer = (elements, state, title, i18Instance) => {
         modalTitle.textContent = post.title;
         modalBody.textContent = post.description;
 
-        // Пометить пост как прочитанный
         if (!state.form.readPosts.includes(post.id)) {
           state.form.readPosts.push(post.id);
           a.classList.replace('fw-bold', 'fw-normal');
@@ -107,116 +102,11 @@ const render = (state, elements, i18Instance) => (path, value) => {
     }
     case 'form.posts': {
       makeContainer(elements, state, 'posts', i18Instance);
-      // attachPreviewHandlers(state);
       break;
     }
     default:
       break;
   }
 };
-
-// const makeContainer = (elements, state, title, i18Instance) => {
-//   elements[title].textContent = '';
-
-//   const card = document.createElement('div');
-//   card.classList.add('card', 'border-0');
-//   const cardBody = document.createElement('div');
-//   cardBody.classList.add('card-body');
-//   const cardTitle = document.createElement('h2');
-//   cardTitle.classList.add('card-title', 'h4');
-//   cardTitle.textContent = i18Instance.t(title);
-//   cardBody.append(cardTitle);
-//   card.append(cardBody);
-
-//   const listGroup = document.createElement('ul');
-//   listGroup.classList.add('list-group', 'border-0', 'rounded-0');
-
-//   if (title === 'feeds') {
-//     state.form.feeds.forEach((feed) => {
-//       const listGroupItem = document.createElement('li');
-//       listGroupItem.classList.add('list-group-item', 'border-0', 'border-end-0');
-//       const h3 = document.createElement('h3');
-//       h3.classList.add('h6', 'm-0');
-//       h3.textContent = feed.title;
-//       const p = document.createElement('p');
-//       p.classList.add('m-0', 'small', 'text-black-50');
-//       p.textContent = feed.description;
-//       listGroupItem.append(h3, p);
-//       listGroup.append(listGroupItem);
-//     });
-//   }
-//   if (title === 'posts') {
-//     state.form.posts.forEach((post) => {
-//       const listGroupItem = document.createElement('li');
-//       listGroupItem.classList.add('list-group-item', 'd-flex',
-// 'justify-content-between', 'align-items-start',
-// 'border-0', 'border-end-0');
-
-//       const a = document.createElement('a');
-//       // Проверка, прочитан ли пост
-//       const isRead = state.form.readPosts.includes(post.id);
-//       a.classList.add(isRead ? 'fw-normal' : 'fw-bold');
-
-//       // a.classList.add('fw-bold')
-//       a.setAttribute('data-id', `${post.id}`);
-//       a.setAttribute('target', '_blank');
-//       a.setAttribute('href', `${post.link}`);
-//       a.setAttribute('rel', 'noopener noreferrer');
-//       a.textContent = post.title;
-
-//       const button = document.createElement('button');
-//       button.classList.add('btn', 'btn-outline-primary', 'btn-sm');
-//       button.setAttribute('data-id', `${post.id}`);
-//       button.setAttribute('data-bs-toggle', 'modal');
-//       button.setAttribute('data-bs-target', '#modal');
-//       button.textContent = i18Instance.t('show');
-
-//       button.addEventListener('click', () => {
-//         const modalTitle = document.querySelector('.modal-title');
-//         const modalBody = document.querySelector('.modal-body');
-//         modalTitle.textContent = post.title;
-//         modalBody.textContent = post.description;
-
-//         // Пометить пост как прочитанный
-//         if (!state.form.readPosts.includes(post.id)) {
-//           state.form.readPosts.push(post.id);
-//           a.classList.replace('fw-bold', 'fw-normal');
-//         }
-//       });
-
-//       listGroupItem.append(a, button);
-//       listGroup.append(listGroupItem);
-//     });
-//   }
-//   card.append(listGroup);
-//   elements[title].append(card);
-// };
-
-// const renderModalWindow = (elements, state, value) => {
-//     elements[value].textContent = '';
-
-//     const modelTitle = document.querySelector('.modal-title');
-//     const modalBody = document.querySelector('.modal-body');
-
-//     state.form.posts.forEach((post) => {
-//         modelTitle.textContent = post.title;
-//         modalBody.textContent = post.description;
-//     })
-// }
-
-// const attachPreviewHandlers = (state) => {
-//     const buttons = document.querySelectorAll('button[data-bs-target="#modal"]');
-//     buttons.forEach((button) => {
-//         button.addEventListener('click', (e) => {
-//             const postId = e.target.getAttribute('data-id');
-//             const post = state.form.posts.find(p => p.id === postId);
-//             const modalTitle = document.querySelector('modal-title');
-//             const modalBody = document.getElementById('modal-body');
-
-//             modalTitle.textContent = post.title;
-//             modalBody.textContent = post.description;
-//         });
-//     });
-// }
 
 export default render;
